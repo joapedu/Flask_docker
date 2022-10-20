1d2b81a2610790bf6277c6db2027c47f5af4d94a
joaoeduardo <109396949+joapedu@users.noreply.github.com>
2022-10-20 14:06:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+import time
+
+import redis
+
+from crypt import methods
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 frutas = []
 registros = []
